ba822da11b3b53e17533af9d715968b916e34009
Dillon <techhead404@gmail>
2024-04-10 09:40:56 -0500

    Initial Commit

---
+++ GitLab/GitLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLabApp.swift
+//  GitLab
+//
+//  Created by GREEK, DILLON L. on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
